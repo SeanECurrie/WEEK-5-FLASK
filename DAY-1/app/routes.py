f874d861f4fb3ae04025412112ae46ef56505fa0
SeanECurrie <101378600+SeanECurrie@users.noreply.github.com>
2022-11-28 19:14:55 -0600

    Initial commit

---
+++ DAY-1/app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template
+
+@app.route('/home')
+def home():
+    
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
---
+++ DAY-1/test.py
@@ -0,0 +1,21 @@
+# Sum of Positive
+
+# Create a function that takes in 1 parameter(list of integers) and return the sum of all of the positives ones.
+# Example
+# Input = [1,-4,7,12]
+# Output = 20
+
+# def positive_integer(lst):
+#     positve_lst = []
+#     for i in lst:
+#         if i >= 0:
+#             positve_lst.append(i)
+#     return sum(positve_lst)
+# int = [9,-1,-1,5]
+# print(positive_integer(int))
+
+
+
+    
+        
+
